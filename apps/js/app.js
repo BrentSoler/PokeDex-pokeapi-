6fd2555a115143c7b514558faf6ccaa063d88b9f
Brent Soler <brntsoler@gmail.com>
2022-01-16 16:03:40 +0800

    update

--- apps/js/app.js
+++ apps/js/app.js
@@ -4,110 +4,198 @@ const cred = document.getElementsByClassName("credentials");
 const searchBar = document.querySelector('#searchbar');
 
 const colors = {
-	fire: '#FDDFDF',
-	grass: '#DEFDE0',
-	electric: '#FCF7DE',
-	water: '#DEF3FD',
-	ground: '#f4e7da',
-	rock: '#d5d5d4',
-	fairy: '#fceaff',
-	poison: '#98d7a5',
-	bug: '#f8d5a3',
-	dragon: '#97b3e6',
-	psychic: '#eaeda1',
-	flying: '#F5F5F5',
-	fighting: '#E6E0D4',
-	normal: '#F5F5F5'
+    fire: '#FDDFDF',
+    grass: '#DEFDE0',
+    electric: '#FCF7DE',
+    water: '#DEF3FD',
+    ground: '#f4e7da',
+    rock: '#d5d5d4',
+    fairy: '#fceaff',
+    poison: '#98d7a5',
+    bug: '#f8d5a3',
+    dragon: '#97b3e6',
+    psychic: '#eaeda1',
+    flying: '#F5F5F5',
+    fighting: '#E6E0D4',
+    normal: '#F5F5F5'
 };
-const limit = "500";
+const limit = "100";
+let offset = 0;
 let search
+let results = []
+
 
 /**
  * LOAD
  */
 loadAll();
-async function loadAll(){
-    let poke = await fetch("https://pokeapi.co/api/v2/pokemon?limit="+limit)
+async function loadAll() {
+    let poke = await fetch("https://pokeapi.co/api/v2/pokemon?limit=" + limit)
     let pokemon = await poke.json();
 
-    loadPoke(pokemon.results)
-}
-
-
-function loadPoke(data){
-    main.innerHTML="";
-
-    let display = data.map(function(pokemon){
+    let display = pokemon.results.map(function (pokemon) {
         return pokemon.url
     })
-    
+
     let i;
     let index = 0;
 
-    for ( i = 0 ; i <= display.length; i++){
-        fetch (display[i])
-        .then(res=>res.json())
-        .then(json=>{
-            let pokeArray = Object.values(json);
-            let pokeName = pokeArray[10];
-            let pokeIMG = pokeArray[14].front_default;
-            let pokeType = pokeArray[16][0].type.name
+    for (i = 0; i <= display.length; i++) {
+        fetch(display[i])
+            .then(res => res.json())
+            .then(json => {
+                let pokeArray = Object.values(json);
+                results.push(pokeArray);
 
-            const showPoke = document.importNode(template.content,true);
+            })
+    }
+    // loadPoke(results)
+    console.log(results)
+}
 
-            const postName = showPoke.querySelector(".name");
-            const postImg = showPoke.querySelector(".pokeimg");
-            const postType = showPoke.querySelector(".type");
+function loadPoke(data) {
+    main.innerHTML = "";
 
-            postName.innerText = pokeName.toUpperCase();
-            postType.innerText = `TYPE:${pokeType.toUpperCase()}`;
-            postImg.src = pokeIMG;
-        
-            main.appendChild(showPoke);
-            cred[index].setAttribute("data-pokemonid",pokeName);
-            cred[index].style.backgroundColor = colors[pokeType];
-            index = index + 1;
-            if(index === limit-1){
-                onclick();
-            }
-        })
+    let display = data.map(function (pokemon) {
+        return pokemon.url
+    })
+
+    let i;
+    let index = 0;
+
+    for (i = 0; i <= display.length; i++) {
+        fetch(display[i])
+            .then(res => res.json())
+            .then(json => {
+                let pokeArray = Object.values(json);
+                let pokeName = pokeArray[10];
+                let pokeIMG = pokeArray[14].front_default;
+                let pokeType = pokeArray[16][0].type.name
+
+                const showPoke = document.importNode(template.content, true);
+
+                const postName = showPoke.querySelector(".name");
+                const postImg = showPoke.querySelector(".pokeimg");
+                const postType = showPoke.querySelector(".type");
+
+                postName.innerText = pokeName.toUpperCase();
+                postType.innerText = `TYPE:${pokeType.toUpperCase()}`;
+                postImg.src = pokeIMG;
+
+                main.appendChild(showPoke);
+                cred[index].setAttribute("data-pokemonid", pokeName);
+                cred[index].style.backgroundColor = colors[pokeType];
+                index = index + 1;
+                if (index === display.length) {
+                    onclick();
+                }
+            })
     }
 }
 
 /**
  * SEARCH
  */
-function searchFor(){
+function searchFor() {
     search = searchBar.value;
 
-    fetch("https://pokeapi.co/api/v2/pokemon?limit="+limit)
-        .then (res => res.json())
-        .then (json =>{
-            const filterOut = json.results.filter(function(pokemon){
-                return (pokemon.name.includes(search)) ? pokemon : (search === "") ? pokemon : null
-            })
-            loadPoke(filterOut)
-        })
+    const filterOut = results.filter(function (pokemon) {
+        return (pokemon.name.includes(search)) ? pokemon : (search === "") ? pokemon : null
+    })
+    loadPoke(filterOut)
+
 
 }
 
-searchBar.addEventListener('keyup',function(){
-    (event.keyCode === 13) ? searchFor() : null;
+searchBar.addEventListener('keyup', function (e) {
+    searchFor();
 })
 
 /**
  * On Click
  */
-function onclick(){
-    for(let i = 0 ; i< cred.length; i++){
-        cred[i].addEventListener("click",()=>{
+let xButt;
+
+
+function onclick() {
+    for (let i = 0; i < cred.length; i++) {
+        cred[i].addEventListener("click", () => {
             let pokemonName = cred[i].getAttribute("data-pokemonid");
-            
-            console.log(pokemonName);
+
+            showPokemon(pokemonName);
         })
     }
 }
 
-function showPokemon(){
+function showPokemon(x) {
+    const pokeContain = document.querySelector('.pokeContainer');
+    const pokeName = document.querySelector('.pokeName');
+    const pokeImg = document.querySelector('.pokeimg');
+    const hpProg = document.querySelector('.hpProg');
+    const atkProg = document.querySelector('.atkProg');
+    const dfnProg = document.querySelector('.dfnProg');
+    const satkProg = document.querySelector('.satkProg');
+    const sdfnProg = document.querySelector('.sdfnProg');
+    const spdProg = document.querySelector('.spdProg');
+    const hpstat = document.querySelector('.hpstat');
+    const atkstat = document.querySelector('.atkstat');
+    const dfnstat = document.querySelector('.dfnstat');
+    const satkstat = document.querySelector('.satkstat');
+    const sdfnstat = document.querySelector('.sdfnstat');
+    const spdstat = document.querySelector('.spdstat');
 
+    const w = document.querySelector('.weight');
+    const h = document.querySelector('.height');
+
+    fetch(`https://pokeapi.co/api/v2/pokemon/${x}`)
+        .then(res => res.json())
+        .then(json => {
+            let pokeArray = Object.values(json);
+            let name = pokeArray[10].toUpperCase();
+            let image = pokeArray[14].front_default;
+            let height = pokeArray[4];
+            let weight = pokeArray[17];
+            let hp = pokeArray[15][0].base_stat;
+            let atk = pokeArray[15][1].base_stat;
+            let dfn = pokeArray[15][2].base_stat;
+            let satk = pokeArray[15][3].base_stat;
+            let sdfn = pokeArray[15][4].base_stat;
+            let spd = pokeArray[15][5].base_stat;
+
+            console.log(pokeArray)
+
+            pokeName.innerText = name;
+            hpstat.innerText = hp;
+            atkstat.innerText = atk;
+            dfnstat.innerText = dfn;
+            satkstat.innerText = satk;
+            sdfnstat.innerText = sdfn;
+            spdstat.innerText = spd;
+            w.innerText = `Weight:${weight}`;
+            h.innerText = `Height:${height}`;
+
+            hpProg.value = hp;
+            atkProg.value = atk;
+            dfnProg.value = dfn;
+            satkProg.value = satk;
+            sdfnProg.value = sdfn;
+            spdProg.value = spd;
+
+            pokeImg.src = image;
+            pokeContain.setAttribute('data-isVisible', true);
+
+        })
+}
+
+window.addEventListener("click", (e) => {
+    const pokeContain = document.querySelector('.pokeContainer');
+    let attri = pokeContain.getAttribute('data-isVisible');
+    (e.target != pokeContain && attri != "false") ? pokeContain.setAttribute('data-isVisible', false) : null;
+    console.log(results)
+})
+
+
+function closeConatin() {
+    const pokeContain = document.querySelector('.pokeContainer');
+    pokeContain.setAttribute('data-isVisible', false);
 }
